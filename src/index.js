3caee23dc0c5c10efc492538ebfe28388175405f
Carlos Lantigua <clantigua954@gmail.com>
2018-08-17 19:59:43 -0400

    Add service section working on importing font awesome

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-//import App from "./App";
 import Navigation from './Navigation';
 import Header from './Header';
 import AboutMe from './AboutMe';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+import { faCheckSquare, faCoffee } from '@fortawesome/free-solid-svg-icons'
 
 import Services from './Services';
 import registerServiceWorker from "./registerServiceWorker";
+library.add(fab, faCheckSquare, faCoffee);
 
 class App extends Component {
   render() {
